f0ae980966b684f8242e62e01b9e71e934c5ffb8
Uros Relic <urosrelicc@gmail.com>
2026-09-06 22:22:06 +0200

    add ejs as a view engine with public folder

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { urlencoded } = require('express');
+const path = require('node:path');
 
 const app = express();
 
+app.set('view engine', ejs);
+app.set('views', path.join(__dirname, 'views'))
+
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000;
 
