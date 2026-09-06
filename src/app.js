2fc866f1180cac7aee97b3fe6f64e9fb4f655aee
Uros Relic <urosrelicc@gmail.com>
2026-09-06 22:23:56 +0200

    add dev script with .env

--- src/app.js
+++ src/app.js
@@ -4,15 +4,19 @@ const path = require('node:path');
 
 const app = express();
 
-app.set('view engine', ejs);
-app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'OK!' });
+});
+
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
